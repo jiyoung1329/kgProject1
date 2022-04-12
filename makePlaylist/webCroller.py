b0d72bc1980ad23f3d922be72cf246742d03b751
jiyoung1329 <jiyoung112837@gmail.com>
2022-04-12 09:49:11 +0900

    make webcrolling basic code

--- makePlaylist/webCroller.py
+++ makePlaylist/webCroller.py
@@ -1,10 +1,46 @@
+#selenium
+from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.common.keys import Keys
+
+
 from bs4 import BeautifulSoup as bs
 import requests
+import time
+import pandas as pd
+
+# url = 'https://www.youtube.com/results?search_query=tj'
+url = "https://www.youtube.com/watch?v=Qj1Gt5z4zxo&list=PL5rkMpxC5Ex8cD8itOnCtFjx0iTg1TgKY"
+
+browser = webdriver.Chrome('chromedriver.exe')
+browser.get(url)
+time.sleep(5)
 
 
-url = 'https://www.youtube.com/results?search_query=tj'
-response = requests.get(url)
-soup = bs(response.text, 'html.parser')
-test = soup.select("#items > ytd-video-renderer")
-print(test)
+html_source = browser.page_source
+soup = bs(html_source, 'lxml')
 
+# 노래 유튜브 링크 따오기
+videos = soup.select("#wc-endpoint")
+print(videos[0].attrs["href"])
+
+# 제목, 가수, 노레번호
+contents = soup.select("#description > yt-formatted-string > span")
+content = contents[0].get_text()
+tmp = content.split("\n")
+title_singer = tmp[0].split("--")
+title = title_singer[0].strip()
+singer = title_singer[1].strip()
+number = tmp[1].split(".")[1]
+print(title, singer, number)
+time.sleep(10000)
+
+
+
+
+
+
+# try:
+#     browser.find_element_by_css_selector("#dismiss-button > a").click()
+# except:
+#     pass
