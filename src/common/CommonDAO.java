a82a64fdb526c9b8bdea7a7dabd5ffd760428a85
jiyoung1329 <jiyoung112837@gmail.com>
2022-04-19 20:16:46 +0900

    add CommonDAO

---
+++ src/common/CommonDAO.java
@@ -0,0 +1,26 @@
+package common;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class CommonDAO {
+		private static String url ="jdbc:oracle:thin:@kgproject_high?TNS_ADMIN=C:/Wallet_kgProject";
+		private static String user = "admin";
+		private static String pwd = "KGproject1234!";
+		
+	public static Connection makeConnection() {
+		Connection conn = null;
+		
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+			conn = DriverManager.getConnection(url, user, pwd);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return conn;
+	}
+	
+ 
+}
